15a072d72cdba2da8a1b11a679391bbc190f065e
DungeonMaster <71274363+YashBharambay@users.noreply.github.com>
2022-08-28 09:43:15 -0400

    Two methods to print maps

--- Maps.cpp
+++ Maps.cpp
@@ -8,9 +8,16 @@ int main() {
    m.insert({1,"abc"});
    m[3] = "wash";
    m.insert({2 , "yash"});
+    
+//     FIRST METHOD TO PRINT
    map<int , string> :: iterator it;
    for(it = m.begin() ; it!=m.end() ; it++)
    {
        cout << (*it).first << " "<< (*it).second<<endl;
    }
+    
+//    SECOND METHOD TO PRINT   (PR is PAIR)
+    for(auto pr:m){
+    cout << pr.first << " " << pr.second << endl;
+    }
 }
