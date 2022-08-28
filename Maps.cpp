0a61aad033537323220e17143b193aa2ec9c908d
DungeonMaster <71274363+YashBharambay@users.noreply.github.com>
2022-08-28 11:32:10 -0400

    Update Maps.cpp

--- Maps.cpp
+++ Maps.cpp
@@ -1,3 +1,5 @@
+In a map when a new value is added it is based on comparison ie. is it big or small then the prev. so if a 
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -5,7 +7,7 @@ using namespace std;
 int main() {
     // Write C++ code here
    map<int,string> m;
-   m.insert({1,"abc"});
+   m.insert({1,"abc"});                        //O(Log(n))
    m[3] = "wash";
    m.insert({2 , "yash"});
     
@@ -13,11 +15,24 @@ int main() {
    map<int , string> :: iterator it;
    for(it = m.begin() ; it!=m.end() ; it++)
    {
-       cout << (*it).first << " "<< (*it).second<<endl;
+       cout << (*it).first << " "<< (*it).second<<endl;                   //O(nlogn)
    }
     
 //    SECOND METHOD TO PRINT   (PR is PAIR)
     for(auto pr:m){
     cout << pr.first << " " << pr.second << endl;
     }
+    
+//    FIND AN ELEMENT  find() passes the iterator.
+    auto it = m.find(3);
+    if(it==m.end())
+    {
+        cout << "No Value";
+    }else
+    {
+        cout<<(*it).first<<" "<<(*it).second<<endl;
+    }
+    
+//   TO DELETE AN ELEMENT
+    m.erase(3);      O(logn)
 }
